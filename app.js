47a049665199862d59a56183217458db05b65839
Lucas Nascimento <lucas@lapou.com.br>
2017-10-06 22:49:14 -0300

    Remove 'users' do app

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./app/routes/index');
-var users = require('./app/routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './app/public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
